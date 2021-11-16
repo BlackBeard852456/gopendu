9616048d3887d2c029aeca7a563acf0b4dde5cd7
BlackBeard852456 <black.barbe147@gmail.com>
2021-11-16 19:23:40 +0100

    Ajout du dictionnaire de mot et affichage du logo du jeux

--- src/main.go
+++ src/main.go
@@ -3,5 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	
+	var numberOfTry int = 10
+	printLogo()
+}
+
+func printLogo() {
+	fmt.Println("###########################")
+	fmt.Println("\t Pendu")
+	fmt.Println("###########################")
 }
