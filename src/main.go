127956d26c679656e1e8ad5a6d2970d8f7347813
BlackBeard852456 <black.barbe147@gmail.com>
2021-11-16 19:42:27 +0100

    Ajout des fonctions qui permettent de lire le dictionnaire de mot et de renvoyer le nombre de mots qu'il contient

--- src/main.go
+++ src/main.go
@@ -1,14 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
 
 func main() {
-	var numberOfTry int = 10
+	dictionnary := getDictionnaryFile("../words")
 	printLogo()
+	numberOfLineOfDictionnary := getNumberLinesOfDictionnary(dictionnary)
+	fmt.Println(numberOfLineOfDictionnary)
 }
 
 func printLogo() {
 	fmt.Println("###########################")
 	fmt.Println("\t Pendu")
 	fmt.Println("###########################")
+}
+
+func getDictionnaryFile(pathFile string) *os.File {
+	file, err := os.Open(pathFile)
+	if err != nil {
+		log.Fatalf("Problème d'ouverture du dictionnaire : %s", err)
+		os.Exit(1)
+	}
+	return file
+}
+
+func getNumberLinesOfDictionnary(wordDictionnary *os.File) int {
+	var numberOfLineOfDictionnary int = 0
+	fileScanner := bufio.NewScanner(wordDictionnary)
+	for fileScanner.Scan() {
+		numberOfLineOfDictionnary++
+	}
+	if err := fileScanner.Err(); err != nil {
+		log.Fatalf("Impossible de lire le dictionnaire : %s", err)
+	}
+	return numberOfLineOfDictionnary
 }
